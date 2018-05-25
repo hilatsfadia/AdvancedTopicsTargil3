6c3c453036836bac40d5528a9660b0d50ea16d87
hilatsfadia <hilatsfadia@gmail.com>
2018-05-25 19:15:57 +0300

    RPSPiece documentation

--- src/ExtendedRPSGame/ExtendedRPSGame/RPSPiece.h
+++ src/ExtendedRPSGame/ExtendedRPSGame/RPSPiece.h
@@ -5,22 +5,24 @@
 // RPSPiece Interface
 //--------------------------
 
+// Abstract class to the RPS game pieces. Manages the characteristics 
+// of the rock paper scissors game piece. A piece of rock paper scissors 
+// game should inherit from this class.
 
 // @author Hila Tsfadia, Jael Fafner TAU - Advanced Topics in Programming - 2018 Semester B
 
 #include "Piece.h"
 #include <memory>
 
-// An abstract class to the RPS game pieces.
 class RPSPiece :
 	public Piece
 {
 public:
 	RPSPiece(std::shared_ptr<Player> owner = nullptr) : Piece(owner) {}
 	RPSPiece(int ownerNum) : Piece(ownerNum) {}
+	virtual ~RPSPiece() {}
 
 	virtual WinningPiece FightWithOtherPieceType(Piece& enemy) override;
-	virtual ~RPSPiece() {}
 	virtual bool IsStrongerThan(const Piece& other) const override;
 };
 
